a4fec4faa6eb39d3dfcacff098d1f31c219fb2b3
Zhigang Wei <84277047+Gary-Zhigang@users.noreply.github.com>
2021-09-18 21:29:52 -0400

    Update bitwise_or.v
    Completed the biewise_or module

--- bitwise_or.v
+++ bitwise_or.v
@@ -2,8 +2,13 @@
  this module implements bitwise OR operation for 32 bit vectors
  assign out = inA | inB;
  */
-module bitwise_or (input [31:0] inA,
-                   input [31:0] inB,
-                   output [31:0] out);
-assign out = 32'h00000000;
+module bitwise_and (input [31:0] inA,
+                    input [31:0] inB,
+                    output [31:0] out);						  
+		genvar i;
+		generate 						  
+  for(i=0; i<32; i = i+1) begin: gen_bitwise_or
+		and u_or (out[i], inA[i], inB[i]);					  
+		end						  
+		endgenerate
 endmodule
