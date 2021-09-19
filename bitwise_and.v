9bb8ac049e46c9e8cf54603075ded993209acbbe
Zhigang Wei <84277047+Gary-Zhigang@users.noreply.github.com>
2021-09-18 21:26:46 -0400

    Update bitwise_and.v
    Completed bitwise_and module

--- bitwise_and.v
+++ bitwise_and.v
@@ -5,5 +5,14 @@
 module bitwise_and (input [31:0] inA,
                     input [31:0] inB,
                     output [31:0] out);
-assign out = 32'h00000000;
+						  
+						  genvar i;
+						  generate 
+						  
+						  for(i=0; i<32; i = i+1) begin: gen_bitwise_and
+						  and u_and (out[i], inA[i], inB[i]);					  
+						  end
+						  
+						  endgenerate
+
 endmodule
